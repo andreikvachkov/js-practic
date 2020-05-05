2c789ab20e1bb1bd3aeb3b51a410094ae1cb13a5
Andrey <andrei.kvachkov@gmail.com>
2020-05-05 20:00:33 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// console.log("hello");
+// alert("hello world");
+
+// let answer = confirm("пидор или нет?");
+
+// console.log(answer);
+
+// let answer = prompt("пидор или нет?", "да");
+
+// console.log(typeof(answer)); 
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(decr++);
+
+console.log(5%2);

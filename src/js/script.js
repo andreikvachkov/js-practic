5251321b54a731151f5c56cbc2516d0ef16da70b
Andrey <andrei.kvachkov@gmail.com>
2020-05-05 20:37:13 +0700

    2 commit

--- src/js/script.js
+++ src/js/script.js
@@ -9,11 +9,64 @@
 
 // console.log(typeof(answer)); 
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 
-console.log(++incr);
-console.log(decr++);
+// console.log(++incr);
+// console.log(decr++);
 
-console.log(5%2);
+// console.log(5%2);
+
+
+// if (2*4 == 7) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+
+
+
+// // if (num < 49) {
+// //     console.log("no")
+// // } else if (num > 100) {
+// //     console.log("Много")
+// // } else {
+// //     console.log("yes")
+// // }
+
+// // (num == 50) ? console.log("Верно") : console.log("Неверно");
+
+// switch (num) {
+//     case num < 49:
+//         console.log("Неверно");
+//         break;
+//     case num > 100:
+//         console.log("Много");
+//         break;
+//     case 50:
+//         console.log("Верно")
+//         break;
+//     default:
+//         console.log("шото не то");
+//         break;
+// }
+
+
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55)
+
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
